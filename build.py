0f0f9722cfc8702cb9347c7c241ad524188cc913
Uilian Ries <uilianries@gmail.com>
2018-05-31 22:17:29 -0300

    Add extra Conan remote to solve jsoncpp
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import os
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(pure_c=False)
+    # TODO (uilianries): Remove after to integrate jsoncpp/1.0.0 on theirix repository
+    os.environ["CONAN_REMOTES"] = os.getenv("CONAN_REMOTE", "https://api.bintray.com/conan/uilianries/conan")
 
+    builder = build_template_default.get_builder(pure_c=False)
     builder.run()
