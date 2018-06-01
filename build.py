3f4dabf177ca33750df131a7201733fdb4b9e712
Uilian Ries <uilian@khomp.com>
2018-06-01 13:47:38 -0300

    Use official theirix repo
    Signed-off-by: Uilian Ries <uilian@khomp.com>

--- build.py
+++ build.py
@@ -5,9 +5,5 @@ import os
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-
-    # TODO (uilianries): Remove after to integrate jsoncpp/1.0.0 on theirix repository
-    os.environ["CONAN_REMOTES"] = os.getenv("CONAN_REMOTE", "https://api.bintray.com/conan/uilianries/conan")
-
     builder = build_template_default.get_builder(pure_c=False)
     builder.run()
